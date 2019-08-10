586db3cc485f522a61621fa0d956f054526dc3fa
Sanjay 1906 <sanjaythakur@venom.com>
2019-08-10 23:56:34 +0530

    my Adminpage

---
+++ README.md
@@ -0,0 +1,12 @@
+# Quoradmin
+
+## Language Used :-
+
+materialize css laibtary
+html
+css
+js
+
+### Admin Page
+
+![](https://raw.githubusercontent.com/sanjay1906/quoradmin/master/images/quoradim.png)
--- js/home.js
+++ js/home.js
@@ -1,4 +1,31 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.slider');
-    var instances = M.Slider.init(elems, );
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    nav: false,
+    margin: 30,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
+
+
+var owl = $(".sliderWapper");
+owl.owlCarousel();
+// Go to the next item
+$(".nxtBtn").click(function () {
+    owl.trigger("next.owl.carousel");
+});
+// Go to the previous item
+$(".prvBtn").click(function () {
+    // With optional speed parameter
+    // Parameters has to be in square bracket '[]'
+    owl.trigger("prev.owl.carousel", [300]);
 });
